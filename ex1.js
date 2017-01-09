117eb021377d80d75045db04a7be6d5a035c7bf7
Curtis Mitchell <cmitchell@CMitchell-282.local>
2017-01-09 10:06:39 -0800

    Solution to ex1

--- ex1.js
+++ ex1.js
@@ -3,15 +3,20 @@
   wrap around `foo(..)`.
  */
 
-function foo(x) {
-  y++;
-  z = x * y;
+function bar(x, y) {
+  var z;
+
+  foo(x);
+
+  return [y, z];
+
+  function foo(x) {
+    y++;
+    z = x * y;
+  }
 }
 
-var y = 5, z;
+bar(20, 5); // [6, 120]
 
-foo(20);
-z;  // 120
-
-foo(25);
-z;  // 175
+bar(20, 5); // [6, 120]
+// same returned value because `bar` is a pure function, unlike `foo`
