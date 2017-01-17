2f33f4e30aca5ea7ae3febc0aba9d9ddf707ee89
Curtis Mitchell <cmitchell@CMitchell-282.local>
2017-01-17 12:25:45 -0800

    Solution to ex3

--- ex3.js
+++ ex3.js
@@ -3,8 +3,11 @@
   work on as many arguments as necessary
  */
 
-function mult(x, y, z) {
-  return x * y * z;
+function mult(...args) {
+  if (args.length <= 2) {
+    return args[0] * args[1];
+  }
+  return args[0] * mult(...args.slice(1));
 }
 
 mult(3, 4, 5); // 60
